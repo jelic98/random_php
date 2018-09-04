53f992ea5cd17cdae2984b061f6d7283b5b8c3cc
Lazar Jelic <lazarjelic98@gmail.com>
2018-09-04 23:00:46 +0200

    Added Instagram unfollowers scrapper

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ Basic scrapper for Google Images thumbnails written in PHP.
 * Get full-resolution image
 * Limit number of images
 
+### Unfollow
+Generates list of Instagram users that you follow, but don't follow you back and opens their profiles in separate windows. Useful if you want to take control in your hands and not get banned for exceeding daily unfollow limit because of bots.
+
 ### Store
-
 Webshop that allows users to create their own virtual stores.
-
 Written a few years ago when I was beginning with PHP.
 
 ### Blog
-
 Complete blog website written in PHP and MySQL.
---
+++ unfollow/instagram.php
@@ -0,0 +1,64 @@
+<?php
+// PLEASE EDIT THESE VARIABLES !!!
+$USERNAME = "dzoks_official";
+$PASSWORD = "dzokssocks2018!";
+$CACHE_PATH = "/Applications/MAMP/htdocs/random_php/unfollow/cache";
+$FOLLOWERS_COUNT = 400; // total followers to extract
+$FOLLOWERS_ROUND = 100; // total followers to extract in one request (must be lower than FOLLOWERS_COUNT)
+$FOLLOWING_COUNT = 10; // total following users to extract
+$FOLLOWING_ROUND = 10; // total following users to extract in one request (must be lower than FOLLOWING_COUNT)
+// DO NOT EDIT ANYTHING BELOW THIS LINE !!!
+// ...it just works ;)
+?>
+<html>
+	<head>
+		<title>Please wait...</title>
+	</head>
+	<body>
+		<script>
+			function openWindows() {
+				<?php
+					require("vendor/autoload.php");
+
+					$index = 0;
+	
+					$followers = [];
+					$following = [];
+			
+					$instagram = \InstagramScraper\Instagram::withCredentials(
+						$USERNAME,
+						$PASSWORD,
+						$CACHE_PATH);
+					$instagram->login();
+					sleep(2);
+
+					$account = $instagram->getAccount($USERNAME);
+					sleep(1);
+
+					$followers = $instagram->getFollowers($account->getId(), $FOLLOWERS_COUNT, $FOLLOWERS_ROUND, true);
+					sleep(2);
+	
+					$followings = $instagram->getFollowing($account->getId(), $FOLLOWING_COUNT, $FOLLOWING_ROUND, true);
+
+					foreach($followings as $following) {
+						$found = false;
+
+						foreach($followers as $follower) {
+							if(strcmp($follower['username'], $following['username']) == 0) {
+								$found = true;
+								break;
+							}
+						}
+
+						if(!$found) {
+							echo 'console.log("' . ++$index . '. ' . $following['username'] . '");';
+							echo 'window.open("https://www.instagram.com/' . $following['username'] . '", "mywindow' . $index . '", "width=500,height=500");';
+						}
+					}
+				?>
+			}
+		</script>
+		<a href="javascript: openWindows()">View unfollowers</a>
+		<p>This link opens each unfollower's profile in new window. You can also see unfollower list in console.</p>
+	</body>
+</html>
