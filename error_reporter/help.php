ed612cbc42c01dc18c8507b55f5e724661a6743b
Lazar Jelic <lazarjelic98@gmail.com>
2018-11-02 18:28:01 +0100

    Added style to help form

--- error_reporter/help.php
+++ error_reporter/help.php
@@ -1,9 +1,96 @@
 <html>
-	<body>
-		<form action="contact.php" method="GET">
-			<input type="text" name="email" placeholder="Email"/>
-			<textarea name="message" placeholder="Message"></textarea>
-			<input type="submit" value"Submit"/>
-		</form>
-	</body>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+    box-sizing: border-box;
+}
+
+input[type=text], select, textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+}
+
+label {
+    padding: 12px 12px 12px 0;
+    display: inline-block;
+}
+
+input[type=submit] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    float: right;
+	margin-top: 10px;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+.container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+	width: 50%;
+	margin: 0 auto;
+}
+
+.col-25 {
+    float: left;
+    width: 25%;
+    margin-top: 6px;
+}
+
+.col-75 {
+    float: left;
+    width: 75%;
+    margin-top: 6px;
+}
+
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+@media screen and (max-width: 600px) {
+    .col-25, .col-75, input[type=submit] {
+        width: 100%;
+        margin-top: 0;
+    }
+}
+</style>
+</head>
+<body>
+<div class="container">
+	<form action="contact.php" method="GET">
+    <div class="row">
+      <div class="col-25">
+        <label for="email">Email</label>
+      </div>
+      <div class="col-75">
+        <input type="text" id="email" name="email" placeholder="Your email...">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+        <label for="message">Question</label>
+      </div>
+      <div class="col-75">
+        <textarea id="message" name="message" placeholder="Your question..." style="height:200px"></textarea>
+      </div>
+    </div>
+    <div class="row">
+      <input type="submit" value="Submit">
+    </div>
+  </form>
+</div>
+</body>
 </html>
