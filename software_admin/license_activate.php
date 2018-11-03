27c2d57c5232c8214d81e209f49f0936f38c2ec7
Lazar Jelic <lazarjelic98@gmail.com>
2018-11-03 23:12:22 +0100

    Added testing license key

--- software_admin/license_activate.php
+++ software_admin/license_activate.php
@@ -5,21 +5,23 @@
 
 	$code = strip($_POST["code"]);
 
-	$cmd = "SELECT activated FROM licenses WHERE code='$code'";
-	$activated = mysqli_fetch_array(mysqli_query($connect, $cmd))[0];
+	if(strcmp($code, "TESTCODE") != 0) {	
+		$cmd = "SELECT activated FROM licenses WHERE code='$code'";
+		$activated = mysqli_fetch_array(mysqli_query($connect, $cmd))[0];
 
-	if(!isset($activated)) {
-		response(400, "Invalid license code");
-		exit;
+		if(!isset($activated)) {
+			response(400, "Invalid license code");
+			exit;
+		}
+
+		if($activated == 1) {
+			response(400, "License already activated");
+			exit;
+		}
+
+		$cmd = "UPDATE licenses SET activated=1 WHERE code='$code'";
+		mysqli_query($connect, $cmd);
 	}
-
-	if($activated == 1) {
-		response(400, "License already activated");
-		exit;
-	}
-
-	$cmd = "UPDATE licenses SET activated=1 WHERE code='$code'";
-	mysqli_query($connect, $cmd);
 
 	response(200, "License successfully activated");
 ?>
