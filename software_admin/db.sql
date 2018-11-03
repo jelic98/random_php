75a6d47c2fb28c3d4d66618f22941da10e1fbbb9
Lazar Jelic <lazarjelic98@gmail.com>
2018-11-03 19:52:10 +0100

    Added database scheme

--- software_admin/db.sql
+++ software_admin/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Nov 01, 2018 at 07:38 PM
+-- Generation Time: Nov 03, 2018 at 07:51 PM
 -- Server version: 5.5.42-log
 -- PHP Version: 7.0.0
 
@@ -11,27 +11,48 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `error_reporter`
+-- Database: `software_admin`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contact`
+-- Table structure for table `help`
 --
 
-CREATE TABLE `contact` (
+CREATE TABLE `help` (
   `id` int(11) NOT NULL,
   `email` varchar(100) COLLATE utf8_bin NOT NULL,
   `message` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `help`
+--
+
+INSERT INTO `help` (`id`, `email`, `message`) VALUES
+(1, 'test@test.com', 'hello there'),
+(2, 'asd@dmc.om', 'asdasd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `licenses`
+--
+
+CREATE TABLE `licenses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `code` varchar(50) COLLATE utf8_bin NOT NULL,
+  `activated` tinyint(1) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `contact`
+-- Dumping data for table `licenses`
 --
 
-INSERT INTO `contact` (`id`, `email`, `message`) VALUES
-(1, 'test@test.com', 'hello there');
+INSERT INTO `licenses` (`id`, `name`, `code`, `activated`) VALUES
+(1, 'Lazar Jelic', 'lazarjelic1', 0);
 
 -- --------------------------------------------------------
 
@@ -77,9 +98,15 @@ INSERT INTO `reports` (`id`, `project`, `message`, `resolved`) VALUES
 --
 
 --
--- Indexes for table `contact`
+-- Indexes for table `help`
 --
-ALTER TABLE `contact`
+ALTER TABLE `help`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `licenses`
+--
+ALTER TABLE `licenses`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -101,9 +128,14 @@ ALTER TABLE `reports`
 --
 
 --
--- AUTO_INCREMENT for table `contact`
+-- AUTO_INCREMENT for table `help`
 --
-ALTER TABLE `contact`
+ALTER TABLE `help`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `licenses`
+--
+ALTER TABLE `licenses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `projects`
