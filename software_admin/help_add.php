695e837070694ce92b8a478ed9ac86378ffd6dc7
Lazar Jelić <lazarjelic98@gmail.com>
2021-03-18 11:39:24 +0100

    Update help_add.php

--- software_admin/help_add.php
+++ software_admin/help_add.php
@@ -8,6 +8,14 @@
 	
 	$cmd = "INSERT INTO help (email, message) VALUES('$email', '$message')";
 	mysqli_query($connect, $cmd);
+	
+	$message = str_replace("\\r\\n", "", $message);
+
+	$headers = "From: " . $email . "\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+	$headers .= "Reply-To: " . $email . "\r\n";
+
+	mail("jdjokovic@raf.rs", "Help", $message, $headers);
 
 	echo "Message successfully sent";
 ?>
